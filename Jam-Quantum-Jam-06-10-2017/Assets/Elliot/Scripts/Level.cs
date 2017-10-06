da4a56621be30bb259cb80181e9d32f76f41bf29
Elliot <elliot@nsquaredsolutions.com>
2017-10-07 10:15:11 +1100

    [Initialise Elliot Branch]

---
+++ Jam-Quantum-Jam-06-10-2017/Assets/Elliot/Scripts/Level.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public GameObject cube;
+    public Vector3 position;
+    public GameObject[] objects;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
