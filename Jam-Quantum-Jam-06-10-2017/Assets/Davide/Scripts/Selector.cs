c84588044b8de6372e35152db9adea72f09f40b0
DESKTOP-PUMICE8\nsquaredAdmin <bobir@nsquaredsolutions.com>
2017-10-07 17:44:11 +1100

    Menu done

---
+++ Jam-Quantum-Jam-06-10-2017/Assets/Davide/Scripts/Selector.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Selector : MonoBehaviour {
+
+    public GameObject[] lights;
+    int lightActive;
+
+	// Use this for initialization
+	void Start ()
+    {
+        lightActive = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.S))
+        {
+            if (lightActive < 2)
+            {
+                lightActive++;
+                foreach (var light in lights)
+                {
+                    light.SetActive(false);
+                }
+                lights[lightActive].SetActive(true);
+            }          
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if(lightActive > 0)
+            {
+                lightActive--;
+                foreach (var light in lights)
+                {
+                    light.SetActive(false);
+                }
+                lights[lightActive].SetActive(true);
+            }         
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (lightActive == 0)
+            {
+                SceneManager.LoadScene("Elliot_Scene");
+            }
+            if (lightActive == 1)
+            {
+                //TODO add CreditScene
+                SceneManager.LoadScene("");
+            }
+            if (lightActive == 2)
+            {
+                Application.Quit();
+            }
+        }
+
+    }
+}
