c5267adfe19dd3023cb5c9d161a361d322691f4a
Elliot <elliot@nsquaredsolutions.com>
2017-10-08 14:24:58 +1100

    [Fix to Animations & Prefabing of Player] - Player Death animation also added.

---
+++ Jam-Quantum-Jam-06-10-2017/Assets/Elliot/Scripts/PlayerGraphic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGraphic : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayerJump()
+    {
+        this.transform.parent.GetComponent<PlayerScript>().StopJump();
+    }
+}
